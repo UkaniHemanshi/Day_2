dd98af02fb70c407e4d47f69b075f6e48872576d
Hemanshi Ukani <87408833+UkaniHemanshi@users.noreply.github.com>
2024-09-17 18:04:07 +0530

    Add files via upload

--- day_14/CSV/csv_app.py
+++ day_14/CSV/csv_app.py
@@ -16,15 +16,25 @@ def product():
     df = pd.read_csv(csv_path)
     for column_name in df.columns:
         list.append(column_name)
+
+    length = len(df)
+    # print(df.iloc[:length])
     session['Header'] = list
+    Rows = df.values.tolist()
+    print(Rows)
+    session['Row'] = Rows
+
+
 
     return redirect(url_for('csv_to_dataframe'))
 
 @appcsv.route('/csvdata')
 def csv_to_dataframe():
     column_names = session.get('Header', [])
+    Rows = session.get('Row', [])
 
-    return render_template("csv_datatable.html",columns = column_names)
+
+    return render_template("csv_datatable.html",columns = column_names,rows = Rows)
 
 
 if __name__ == '__main__':
